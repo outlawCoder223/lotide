d0deb04ca5b909bf62e5cbb2e2347403ef27c1ad
Rance Campbell <rancewcampbell@gmail.com>
2020-03-31 21:41:00 +0000

    added without function which takes a source array and a remove array. Function returns a new array with the remove items deleted from source.

---
+++ without.js
@@ -0,0 +1,41 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log('✅ Assertion Passed: These arrays are equal');
+  } else {
+    console.log('🛑 Assertion Failed: These arrays are not equal');
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  const newArray = [];
+  for (let item of source) {
+    if (!itemsToRemove.includes(item)) {
+      newArray.push(item);
+    }
+  }
+  return newArray;
+};
+
+// Test Cases:
+
+// Check without() not modifying original array
+const words = [ 'hello', 'world', 'lighthouse' ];
+without(words, [ 'lighthouse' ]);
+assertArraysEqual(words, [ 'hello', 'world', 'lighthouse' ]);
+
+// Ensure type matters
+assertArraysEqual(without([ '1', '2', '3' ], [ 1, 2, '3' ]), [ '1', '2' ]);
+// Function working properly
+assertArraysEqual(without([ 1, 2, 3, 4 ], [ 1, 2 ]), [ 3, 4 ]);
+assertArraysEqual(without([ 'slim', 'shady' ], [ 'eminem' ]), [ 'slim', 'shady' ]);
