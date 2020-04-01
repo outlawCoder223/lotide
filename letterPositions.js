6d8bde068f0be163a7124a81a59752089fe373a4
Rance Campbell <rancewcampbell@gmail.com>
2020-04-01 20:07:32 +0000

    creates object containing each letter in a sentence and the index or indices of that letter

---
+++ letterPositions.js
@@ -0,0 +1,49 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log('✅ Assertion Passed: These arrays are equal');
+  } else {
+    console.log('🛑 Assertion Failed: These arrays are not equal');
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      if (sentence[i] !== ' ') {
+        results[sentence[i]] = [];
+        results[sentence[i]].push(i);
+      }
+    }
+  }
+  return results;
+};
+
+// Test Cases:
+
+const test = letterPositions('slim shady');
+
+assertArraysEqual(test.s, [ 0, 5 ]);
+assertArraysEqual(test.y, [ 9 ]);
+assertEqual(test[' '], undefined);
