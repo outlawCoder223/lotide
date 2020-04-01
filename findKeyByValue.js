2b36ca619ea27da307a10fd5f5acdbc575b923c3
Rance Campbell <rancewcampbell@gmail.com>
2020-04-01 20:31:21 +0000

    added findKeyByValue which returns first key that matches the value passed it.

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(toSearch, searchVal) {
+  const keys = Object.keys(toSearch);
+  for (let key of keys) {
+    if (toSearch[key] === searchVal) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: 'The Expanse',
+  comedy: 'Brooklyn Nine-Nine',
+  drama: 'The Wire',
+  action: 'Brooklyn Nine-Nine'
+};
+
+// Test Cases:
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, 'The Wire'), 'drama');
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, 'Brooklyn Nine-Nine'), 'comedy');
