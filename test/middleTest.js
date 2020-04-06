7e5726fa39cb2cd3a3f4e7a860e7df6ef65fdc03
Rance Campbell <rancewcampbell@gmail.com>
2020-04-06 15:26:31 +0000

    changed middleTest to mocha testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,16 @@
 // Test for middle.js
-
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([ 1, 2, 3, 4, 5, 6, 7 ]), [ 4 ]);
-assertArraysEqual(middle([ 1, 2, 3, 4, 5, 6 ]), [ 3, 4 ]);
-assertArraysEqual(middle([ 1, 2 ]), []);
-assertArraysEqual(middle([ 'hello', 'cats', 'gregory', 'pewter' ]), [ 'cats', 'gregory' ]);
-assertArraysEqual(middle([]), []);
+describe('#middle', () => {
+  it('returns the middle element of an array of odd length', () => {
+    assert.deepEqual(middle([ 1, 2, 3, 4, 5 ]), [ 3 ]);
+  });
+  it('returns the middle two elements of an array of even length', () => {
+    assert.deepEqual(middle([ 1, 2, 3, 4, 5, 6 ]), [ 3, 4 ]);
+  });
+  it('returns an empty array if an array with a length of two or less is passed', () => {
+    assert.deepEqual(middle([ 1, 2 ]), []);
+    assert.deepEqual(middle([]), []);
+  });
+});
