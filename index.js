eeee96b354de3293983b583e486a8b3bd7553626
Rance Campbell <rancewcampbell@gmail.com>
2020-04-06 16:04:13 +0000

    added eqArrays to index.js

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const tail = require('./tail');
 const middle = require('./middle');
 const countLetters = require('./countLetters');
 const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
 
 module.exports = {
   head: head,
   tail: tail,
   middle: middle,
   countLetters: countLetters,
-  countOnly: countOnly
+  countOnly: countOnly,
+  eqArrays: eqArrays
 };
