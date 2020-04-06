bb95263e59fb21d009b1eaadf2df302a931352c0
Rance Campbell <rancewcampbell@gmail.com>
2020-04-06 15:21:14 +0000

    changed tailTest to mocha testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,22 +1,22 @@
 // Tests for tail.js
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-// Test Case 1: Ensure function does not modify original array
-const numbers = [ 1, 2, 3, 4, 5, 6 ];
-tail(numbers);
-assertEqual(numbers.length, 6);
-
-// Test Case 2: Check returned elements
-const lyrics = [ 'my', 'name', 'is', 'slim', 'shady' ];
-const result = tail(lyrics);
-assertEqual(result.length, 4);
-for (let i = 0; i < result.length; i++) {
-  assertEqual(result[i], lyrics[i + 1]);
-}
-
-// Test Case 3: Empty array
-assertEqual(tail([]).length, 0);
-
-// Test Case 4: One element
-assertEqual(tail([ 1 ]).length, 0);
+describe('#tail', () => {
+  it('does not modify original array', () => {
+    const numbers = [ 1, 2, 3, 4 ];
+    tail(numbers);
+    assert.strictEqual(numbers.length, 4);
+  });
+  it('returns all elements after the first element', () => {
+    const letters = [ 'a', 'b', 'c', 'd' ];
+    const lettersTail = tail(letters);
+    assert.deepEqual(lettersTail, [ 'b', 'c', 'd' ]);
+  });
+  it('returns an empty array when passed an empty array', () => {
+    assert.deepEqual(tail([]), []);
+  });
+  it('returns an empty array if an array with one element is passed', () => {
+    assert.deepEqual(tail([ 1 ]), []);
+  });
+});
