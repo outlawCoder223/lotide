b749e16e892638f51709760d2c402c056964d639
Rance Campbell <rancewcampbell@gmail.com>
2020-04-06 15:45:52 +0000

    adding countLetters to index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  countLetters: countLetters
+};
